0d241361836874e3aa0c65b67fe3385bbeda3244
Sahil Thakur <sahilthakur376@gmail.com>
2021-08-20 22:53:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+    </div>
+  );
+}
---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles.css";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="main-div">
+        <h3>LetsUpgrade Assignment : 1</h3>
+        <p>Task : Add two components to App</p>
+      </div>
+      <footer>
+        <h3>Made By Sahil Thakur.</h3>
+      </footer>
+    </div>
+  );
+};
+
+export default Banner;
